c60de62a27c872ecdce5a5b4ddff34a56768e4a5
William Okta Jaya <williamoktajaya@gmail.com>
2025-11-03 19:55:11 +0800

    Tugas menghitung luas segitiga
    Nama : William Okta Jaya 
    NIM : D0425510
    Prodi : Sistem Informasi

---
+++ Tugas menghitung luas segitiga.py
@@ -0,0 +1,11 @@
+#Nama : William Okta Jaya
+#NIM : D0425510
+#Tugas membuat luas segitiga
+
+alas = float(input("MASUKAN ALAS SEGITIGA :  "))
+
+tinggi = float(input("MASUKAN TINGGI SEGITIGA : "))
+
+luas = (alas * tinggi) / 2
+
+print(f"LUAS SEGITIGA ADALAH : {luas}")
