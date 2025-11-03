70f6181f7a9671f4875150f38f331f916576a2f3
William Okta Jaya <williamoktajaya@gmail.com>
2025-11-03 20:15:39 +0800

    Tugas menghitung luas persegi
    Nama : William Okta Jaya
    NIM : D0425510
    Prodi : Sistem Informasi

---
+++ Tugas menghitung luas persegi.py
@@ -0,0 +1,11 @@
+#Nama : William Okta Jaya
+#NIM : D0425510
+#Tugas menghitung luas persegi
+
+sisi = float(input("MASUKAN SISI PERSEGI :  "))
+
+sisi = float(input("MASUKAN SISI PERSEGI : "))
+
+luas = ( sisi * sisi) 
+
+print(f"LUAS PERSEGI ADALAH : {luas}")
